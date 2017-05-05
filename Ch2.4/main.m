a4d34b3a084794c0ecdfa2d6bbfca68cec4a0e1a
Len512 <echatzak@gmail.com>
2017-05-05 16:16:48 +0200

    Update main.m

--- Ch2.4/main.m
+++ Ch2.4/main.m
@@ -1,1 +1,12 @@
 
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int value1 = 87;
+        int value2 = 15;
+        NSLog(@"%i minus %i is %i", value1, value2, value1 - value2);
+    }
+    return 0;
+}
