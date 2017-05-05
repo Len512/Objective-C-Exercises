ed7784c5ba04cfdaa779c6fdafeb387ce7a4da1e
Len512 <echatzak@gmail.com>
2017-05-05 16:18:56 +0200

    Create main.m

---
+++ Ch2.5/main.m
@@ -0,0 +1,12 @@
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int sum;
+        sum = 25 + 37 - 19;
+        NSLog(@"The answer is %i", sum);
+    }
+    return 0;
+}
