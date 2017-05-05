3a739f50d81dbf1c242e6f377093d689313a07d1
Len512 <echatzak@gmail.com>
2017-05-05 16:21:07 +0200

    Create main.m

---
+++ Ch2.6/main.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int answer, result;
+        answer = 100;
+        result = answer - 10;
+        NSLog(@"The result is %i\n", result + 5);
+    }
+    return 0;
+}
