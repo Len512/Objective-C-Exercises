e5aeab5327b520bcf0672f63b2bb5b2dc212930a
Len512 <echatzak@gmail.com>
2017-05-05 16:09:08 +0200

    Create main.m

---
+++ Ch2.2/main.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSString *message = @"In Objective-C, lowercase letters are significant. \nmain is where program execution begins.\nOpen and closed braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.";
+        NSLog(@"%@", message);
+    }
+    return 0;
+}
