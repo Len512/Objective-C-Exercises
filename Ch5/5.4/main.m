2657f93437e14d104af20bba8375c9f5cf669b6d
Len512 <echatzak@gmail.com>
2017-05-08 14:12:16 +0200

    Update main.m

--- Ch5/5.4/main.m
+++ Ch5/5.4/main.m
@@ -2,16 +2,14 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int factorial = 1;
-        
-        NSLog(@"Table of Factorials");
-        NSLog(@"n   Factorials");
-        NSLog(@"--  ----------");
-        
-        
-        for (int n=1; n <= 10; ++n){
-            factorial *= n;
-            NSLog(@"%-2i  %-7i", n, factorial);
+        int n, triangularNumber;
+        NSLog (@"TABLE OF TRIANGULAR NUMBERS");
+        NSLog (@" n Sum from 1 to n");
+        NSLog (@"-- --------");
+        triangularNumber = 0;
+        for ( n = 1; n <= 10; ++n ) {
+            triangularNumber += n;
+            NSLog (@"%-2i %i", n, triangularNumber);
         }
     }
     return 0;
