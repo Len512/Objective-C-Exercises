ff073b16b22cc106c511e8a31b4d30736b823de1
Len512 <echatzak@gmail.com>
2017-05-08 14:25:12 +0200

    Update 1_main.m

--- Ch5/5.6/1_main.m
+++ Ch5/5.6/1_main.m
@@ -1,5 +1,10 @@
 #import <Foundation/Foundation.h>
-// Program 5.2 to calculate the 200th triangular number
+
+// Program 5.2 to calculate the 200th triangular number 
+// Rewrite Program 5.2 replacing all uses of the for statement with
+// equivalent while statements. Run each program to verify that both versions are
+// identical.
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         int n, triangularNumber;
