ea6c16a9355b0f37184ddc3def1b80ac42bc9626
Len512 <echatzak@gmail.com>
2017-05-08 13:02:57 +0200

    Create Calculator.h

---
+++ Ch4/4.8/Calculator.h
@@ -0,0 +1,17 @@
+#ifndef Calculator_h
+#define Calculator_h
+
+@interface Calculator : NSObject
+{
+    double accumulator;
+}
+- (void) setAccumulator: (double) value;
+- (void) clear;
+- (double) accumulator;
+- (double) add: (double) value;
+- (double) subtract: (double) value;
+- (double) multiple: (double) value;
+- (double) divide: (double) value;
+@end
+
+#endif /* Calculator_h */
