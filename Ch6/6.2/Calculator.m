e77cc8e757559f32b235c942f2227cfdf22f1c12
Len512 <echatzak@gmail.com>
2017-05-08 15:20:06 +0200

    Create Calculator.m

---
+++ Ch6/6.2/Calculator.m
@@ -0,0 +1,27 @@
+#import <Foundation/Foundation.h>
+#import "Calculator.h"
+
+@implementation Calculator
+
+- (void) setAccumulator: (double) value {
+    accumulator = value;
+}
+- (void) clear {
+    accumulator = 0;
+}
+- (double) accumulator {
+    return accumulator;
+}
+- (void) add: (double) value {
+    accumulator += value;
+}
+- (void) subtract: (double) value {
+    accumulator -= value;
+}
+- (void) multiply: (double) value {
+    accumulator *= value;
+}
+- (void) divide: (double) value {
+    accumulator /= value;
+}
+@end
