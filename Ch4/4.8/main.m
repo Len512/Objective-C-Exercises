9b2425c94fe72af4e0eb737c52f431b639088801
Len512 <echatzak@gmail.com>
2017-05-08 13:02:37 +0200

    Create main.m

---
+++ Ch4/4.8/main.m
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+#import "Calculator.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Calculator *myCalc = [ [Calculator alloc] init];
+        [myCalc setAccumulator:13];
+        NSLog(@"Starting with %g", [myCalc accumulator]);
+        NSLog(@"13 + 5 = %g", [myCalc add:5]);
+        NSLog(@"13 - 45 = %g", [myCalc subtract:45]);
+        NSLog(@"13 * 3 = %g", [myCalc multiple:3]);
+        NSLog(@"13 / 2 = %g", [myCalc divide:2]);
+        
+    }
+    return 0;
+}
