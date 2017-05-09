60d6bbfa2c14dfcc06e35648170aa776d42ac397
Len512 <echatzak@gmail.com>
2017-05-09 15:22:05 +0200

    Create Rectangle.h

---
+++ Ch8/8.5/Rectangle.h
@@ -0,0 +1,26 @@
+#ifndef Rectangle_h
+#define Rectangle_h
+
+#import <Foundation/Foundation.h>
+#import "GraphicObject.h"
+
+@class XYPoint;
+
+@interface Rectangle : GraphicObject
+{
+    float width;
+    float height;
+    XYPoint *origin;
+}
+@property float width, height;
+
+- (void) setWidth: (float) w andHeight: (float) h;
+- (float) area;
+- (float) perimeter;
+// defining getter and setter methods for origin
+- (XYPoint *) origin;
+- (void) setOrigin: (XYPoint *) pt;
+- (void) translate: (XYPoint *) pt;
+@end
+
+#endif /* Rectangle_h */
