bfaf72b06e5fa346aafb4cc8b739749d5d7a59b3
Len512 <echatzak@gmail.com>
2017-05-09 15:47:14 +0200

    Create Rectangle.m

---
+++ Ch8/8.6/Rectangle.m
@@ -0,0 +1,42 @@
+#import <Foundation/Foundation.h>
+#import "Rectangle.h"
+#import "XYPoint.h"
+@implementation Rectangle
+
+@synthesize width, height;
+
+- (void) setWidth: (float) w andHeight: (float) h {
+    width = w;
+    height = h;
+}
+- (float) area {
+    return width * height;
+}
+- (float) perimeter {
+    return 2*width + 2*height;
+}
+- (XYPoint *) origin {
+    return origin;
+}
+- (void) setOrigin: (XYPoint *) pt {
+    if (!origin) {
+        origin = [[XYPoint alloc] init];
+    }
+    origin.x = pt.x;
+    origin.y = pt.y;
+}
+- (void) translate: (XYPoint *) pt {
+    if (!origin) {
+        origin = [[XYPoint alloc] init];
+    }
+    origin.x += pt.x;
+    origin.y += pt.y;
+}
+- (BOOL) containsPoint: (XYPoint *) pt {
+    if (pt.x >= origin.x && pt.x <= origin.x + width && pt.x >= origin.y && pt.y <= origin.y + height){
+        return YES;
+    } else {
+        return NO;
+    }
+}
+@end
