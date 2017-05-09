533115e2f5b01b1c95aca26213ff30f1406e7852
Len512 <echatzak@gmail.com>
2017-05-09 12:02:17 +0200

    Create Fraction.h

---
+++ Ch7/7.3/Fraction.h
@@ -0,0 +1,27 @@
+#ifndef Fraction_h
+#define Fraction_h
+
+#import <Foundation/Foundation.h>
+
+// The Fraction Class
+
+@interface Fraction : NSObject
+{
+    int numerator;
+    int denominator;
+}
+@property int numerator, denominator;
+
+- (void) print: (bool) b;
+- (double) convertToNum;
+- (void) setTo: (int) n over: (int) d;
+- (Fraction *) add: (Fraction *) f;
+- (Fraction *) subtract: (Fraction *) f;
+- (Fraction *) multiply: (Fraction *) f;
+- (Fraction *) divide: (Fraction *) f;
+
+- (void) reduce;
+
+@end
+
+#endif /* Fraction_h */
