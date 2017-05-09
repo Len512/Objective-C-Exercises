d884012528f36f7eda29933b17f6882a6047e999
Len512 <echatzak@gmail.com>
2017-05-09 14:34:26 +0200

    Create XYPoint.m

---
+++ Ch8/8.2/XYPoint.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+#import "XYPoint.h"
+@implementation XYPoint
+
+@synthesize x, y;
+
+- (void) setX:(float)xVal andY:(float)yVal {
+    x = xVal;
+    y = yVal;
+}
+
+@end
