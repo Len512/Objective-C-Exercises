a3a0f7eb832baae7151a47f7950899702013dc25
Len512 <echatzak@gmail.com>
2017-05-10 12:35:04 +0200

    Create Fraction.m

---
+++ Ch9/9.2/Fraction.m
@@ -0,0 +1,44 @@
+#import <Foundation/Foundation.h>
+#import "Fraction.h"
+
+@implementation Fraction
+
+@synthesize numerator, denominator;
+
+- (void) print {
+    NSLog(@" %i/%i ", numerator, denominator);
+}
+- (void) setTo: (int) n over: (int) d {
+    numerator = n;
+    denominator = d;
+}
+- (double) convertToNum {
+    if (denominator != 0) {
+        return (double) numerator / denominator;
+    } else {
+        return NAN;
+    }
+}
+- (Fraction *) add: (Fraction *) f {
+    Fraction *result = [[Fraction alloc] init];
+    
+    result.numerator = numerator * f.denominator + denominator * f.numerator;
+    result.denominator = denominator * f.denominator;
+    [result reduce];
+    return result;
+}
+- (void) reduce {
+    int u = numerator;
+    int v = denominator;
+    int temp;
+    
+    while (v != 0) {
+        temp = u % v;
+        u = v;
+        v = temp;
+    }
+    numerator /= u;
+    denominator /= u;
+}
+
+@end
