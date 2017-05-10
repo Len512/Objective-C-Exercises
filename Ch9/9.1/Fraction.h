1210f1f371b1082742910a098337fc4a0ae8a800
Len512 <echatzak@gmail.com>
2017-05-10 12:24:40 +0200

    Create Fraction.h

---
+++ Ch9/9.1/Fraction.h
@@ -0,0 +1,17 @@
+#ifndef Fraction_h
+#define Fraction_h
+
+@interface Fraction : NSObject
+{
+    int numerator;
+    int denominator;
+}
+@property int numerator, denominator;
+- (void) print;
+- (void) setTo: (int) n over: (int) d;
+- (double) convertToNum;
+- (Fraction *) add: (Fraction *) f;
+- (void) reduce;
+@end
+
+#endif /* Fraction_h */
