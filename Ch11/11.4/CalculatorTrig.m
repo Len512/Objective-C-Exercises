9fabfd41a1a70cee6a149efd725c10f7d08bf5c7
Len512 <echatzak@gmail.com>
2017-05-11 10:32:31 +0200

    Create CalculatorTrig.m

---
+++ Ch11/11.4/CalculatorTrig.m
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#import "Calculator.h"
+
+@implementation Calculator (Trig)
+
+- (double) sin {
+    double result = sin(accumulator);
+    return result;
+}
+- (double) cos {
+    double result = cos(accumulator);
+    return result;
+}
+- (double) tan {
+    double result = tan(accumulator);
+    return result;
+}
+
+@end
