7a70becd8dd6aa0e83eb52e3a9ac0b4bb79f3650
Len512 <echatzak@gmail.com>
2017-05-11 10:32:14 +0200

    Create CalculatorTrig.h

---
+++ Ch11/11.4/CalculatorTrig.h
@@ -0,0 +1,14 @@
+#ifndef CalculatorTrig_h
+#define CalculatorTrig_h
+
+#import "Calculator.h"
+
+@interface Calculator (Trig)
+
+- (double) sin;
+- (double) cos;
+- (double) tan;
+
+@end
+
+#endif /* CalculatorTrig_h */
