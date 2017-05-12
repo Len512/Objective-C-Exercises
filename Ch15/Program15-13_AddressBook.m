f37d1fc8bb35a6b69ab8b1ad039b55157595aa89
Len512 <echatzak@gmail.com>
2017-05-12 13:25:06 +0200

    Create Program15-13_AddressBook.m

---
+++ Ch15/Program15-13_AddressBook.m
@@ -0,0 +1,44 @@
+#import <Foundation/Foundation.h>
+#import "AddressBook.h"
+
+@implementation AddressBook
+
+- (id) initWithName:(NSString *) name {
+    self = [super init];
+    if (self) {
+        bookName = [[NSString alloc] initWithString: name];
+        book = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+- (id) init {
+    return [self initWithName:@"NoName"];
+}
+- (void) addCard: (AddressCard *) theCard {
+    [book addObject:theCard];
+}
+- (long int) entries {
+    return [book count];
+}
+- (void) list {
+    NSLog(@"======== Contents of: %@ =========", bookName);
+    for ( AddressCard *theCard in book){
+        NSLog(@"%-20s   %-32s", [theCard.name UTF8String], [theCard.email UTF8String]);
+        NSLog(@"=====================================================");
+    }
+}
+- (AddressCard *) lookup: (NSString *) theName {
+    for (AddressCard *nextCard in book){
+        if ( [nextCard.name caseInsensitiveCompare:theName] == NSOrderedSame){
+            return nextCard;
+        }
+    }
+    return nil;
+}
+- (void) removeCard: (AddressCard *) theCard {
+    [book removeObjectIdenticalTo: theCard];
+}
+- (void) sort {
+    [book sortUsingSelector:@selector(compareNames:)];
+}
+@end
