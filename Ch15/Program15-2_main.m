95cee0a33da736c50f6854b97ce1bdeb4cf5af09
Len512 <echatzak@gmail.com>
2017-05-12 10:38:33 +0200

    Create Program15-2_main.m

---
+++ Ch15/Program15-2_main.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, char *argv[]){
+    @autoreleasepool {
+        
+        NSString *str = @"Programming is fun";
+        NSLog(@"%@", str );
+    }
+    
+    return 0;
+}
