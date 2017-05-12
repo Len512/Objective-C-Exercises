c2bce359a9890d8f0c44d8a4e32e4aebe65d1ae3
Len512 <echatzak@gmail.com>
2017-05-12 13:10:30 +0200

    Create Program15-12_main.m

---
+++ Ch15/Program15-12_main.m
@@ -0,0 +1,54 @@
+// Test program for AddressBook class - Remove a particular Address Card
+
+#import <Foundation/Foundation.h>
+#import "AddressBook.h"
+
+int main (int argc, char *argv[]){
+    @autoreleasepool {
+        
+        NSString *aName = @"Julia Kochan";
+        NSString *aEmail = @"jewls337@axlc.com";
+        NSString *bName = @"Tony Iannino";
+        NSString *bEmail = @"tony.iannino@tech.com";
+        NSString *cName = @"Stephen Kochan";
+        NSString *cEmail = @"steve@classroomM.com";
+        NSString *dName = @"Jamie Baker";
+        NSString *dEmail = @"jbaker@classroomM.com";
+        
+        AddressCard *card1 = [[AddressCard alloc] init];
+        AddressCard *card2 = [[AddressCard alloc] init];
+        AddressCard *card3 = [[AddressCard alloc] init];
+        AddressCard *card4 = [[AddressCard alloc] init];
+        
+        // Set up a new address book
+        AddressBook *myBook = [[AddressBook alloc] initWithName:@"Len512's address book"];
+        AddressCard *myCard;
+        
+        // Set up four new address cards
+        [card1 setName: aName andEmail: aEmail];
+        [card2 setName: bName andEmail: bEmail];
+        [card3 setName: cName andEmail: cEmail];
+        [card4 setName: dName andEmail: dEmail];
+        
+        // Add the cards to the address book
+        [myBook addCard: card1];
+        [myBook addCard: card2];
+        [myBook addCard: card3];
+        [myBook addCard: card4];
+        
+        // Look up a person by name
+        NSLog(@"Looking up Stephen Kochan");
+        myCard  = [myBook lookup:@"stephen kochan"];
+        if (myCard != nil) {
+            [myCard print];
+        } else {
+            NSLog(@"Not found!");
+        }
+        
+        // Now remove the entry from the phone book
+        [myBook removeCard:myCard];
+        [myBook list];
+        
+    }
+    return 0;
+}
