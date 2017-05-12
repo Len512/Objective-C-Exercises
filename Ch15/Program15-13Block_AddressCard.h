8097c197b3ce67da393c1756db8f51afd28a9ab8
Len512 <echatzak@gmail.com>
2017-05-12 13:34:43 +0200

    Create Program15-13Block_AddressCard.h

---
+++ Ch15/Program15-13Block_AddressCard.h
@@ -0,0 +1,17 @@
+#ifndef AddressCard_h
+#define AddressCard_h
+#import <Foundation/Foundation.h>
+
+@interface AddressCard : NSObject
+{
+    NSString *name;
+    NSString *email;
+}
+@property (copy, nonatomic) NSString *name, *email;
+
+- (void) print;
+- (void) setName: (NSString *) theName andEmail: (NSString *) theEmail;
+- (id) initWithName: (NSString *) n andEmail: (NSString *) e;
+- (id) init;
+@end
+#endif /* AddressCard_h */
