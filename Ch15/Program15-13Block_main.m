6b96217c8dbe74bad67563c5d6f1d0ced3a1815b
Len512 <echatzak@gmail.com>
2017-05-12 13:37:03 +0200

    Create Program15-13Block_main.m

---
+++ Ch15/Program15-13Block_main.m
@@ -0,0 +1,47 @@
+// Test program for AddressBook class - Sort Address Book using Comparator with Block
+
+#import <Foundation/Foundation.h>
+#import "AddressBook.h"
+
+int main (int argc, char *argv[]){
+    @autoreleasepool {
+        
+        NSString *aName = @"Julia Kochan";
+        NSString *aEmail = @"jewls337@axlc.com";
+        NSString *bName = @"Tony Iannino";
+        NSString *bEmail = @"tony.iannino@tech.com";
+        NSString *cName = @"Stephen Kochan";
+        NSString *cEmail = @"steve@classroomM.com";
+        NSString *dName = @"Jamie Baker";
+        NSString *dEmail = @"jbaker@classroomM.com";
+        
+        AddressCard *card1 = [[AddressCard alloc] init];
+        AddressCard *card2 = [[AddressCard alloc] init];
+        AddressCard *card3 = [[AddressCard alloc] init];
+        AddressCard *card4 = [[AddressCard alloc] init];
+        
+        // Set up a new address book
+        AddressBook *myBook = [[AddressBook alloc] initWithName:@"Len512's address book"];
+
+        // Set up four new address cards
+        [card1 setName: aName andEmail: aEmail];
+        [card2 setName: bName andEmail: bEmail];
+        [card3 setName: cName andEmail: cEmail];
+        [card4 setName: dName andEmail: dEmail];
+        
+        // Add the cards to the address book
+        [myBook addCard: card1];
+        [myBook addCard: card2];
+        [myBook addCard: card3];
+        [myBook addCard: card4];
+        
+        // List the unsorted address book
+        [myBook list];
+        
+        // Sort it and list it again
+        NSLog(@"This is the sorted address book!");
+        [myBook sort];
+        [myBook list];
+    }
+    return 0;
+}
