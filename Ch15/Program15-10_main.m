53edf40b2c05db2571b6648d4d423c6726f7d025
Len512 <echatzak@gmail.com>
2017-05-12 12:47:28 +0200

    Create Program15-10_main.m

---
+++ Ch15/Program15-10_main.m
@@ -0,0 +1,46 @@
+// Test program for AddressBook class
+
+#import <Foundation/Foundation.h>
+#import "AddressBook.h"
+
+int main (int argc, char *argv[]){
+    @autoreleasepool {
+        
+        NSString *aName = @"Julia Kochan";
+        NSString *aEmail = @"jewls337@axlc.com";
+        NSString *bName = @"Tony Iannino";
+        NSString *bEmail = @"tony.iannino@tech.com";
+        NSString *cName = @"Stephen Kochan";
+        NSString *cEmail = @"steve@classroomM.com";
+        NSString *dName = @"Jamie Baker";
+        NSString *dEmail = @"jbaker@classroomM.com";
+        
+        AddressCard *card1 = [[AddressCard alloc] init];
+        AddressCard *card2 = [[AddressCard alloc] init];
+        AddressCard *card3 = [[AddressCard alloc] init];
+        AddressCard *card4 = [[AddressCard alloc] init];
+        
+        // Set up a new address book
+        AddressBook *myBook = [[AddressBook alloc] initWithName:@"Len512's address book"];
+        
+        // Set up four new address cards
+        [card1 setName: aName andEmail: aEmail];
+        [card2 setName: bName andEmail: bEmail];
+        [card3 setName: cName andEmail: cEmail];
+        [card4 setName: dName andEmail: dEmail];
+        
+        // Add the cards to the address book
+        [myBook addCard: card1];
+        [myBook addCard: card2];
+        [myBook addCard: card3];
+        [myBook addCard: card4];
+        
+        NSLog(@"Entries in address book after adding cards: %li", [myBook entries]);
+        
+        // List all entries in the address book
+        [myBook list];
+        
+        
+    }
+    return 0;
+}
