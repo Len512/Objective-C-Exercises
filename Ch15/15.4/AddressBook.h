bc5503c97d1573e12b3ed8b8a5d35298635666b2
Len512 <echatzak@gmail.com>
2017-05-15 10:27:04 +0200

    Create AddressBook.h

---
+++ Ch15/15.4/AddressBook.h
@@ -0,0 +1,28 @@
+#ifndef AddressBook_h
+#define AddressBook_h
+
+#import <Foundation/Foundation.h>
+#import "AddressCard.h"
+
+@interface AddressBook : NSObject
+{
+    NSString       *bookName;
+    NSMutableArray *book;
+}
+@property NSString *bookName;
+
+- (id) initWithName: (NSString *) name;
+- (id) init;
+
+- (long int) entries;
+- (void) list;
+- (void) sort;
+
+
+- (void) addCard: (AddressCard *) theCard;
+- (void) removeCard: (AddressCard *) theCard;
+
+- (NSMutableArray *) lookup: (NSString *) theName;
+@end
+
+#endif /* AddressBook_h */
