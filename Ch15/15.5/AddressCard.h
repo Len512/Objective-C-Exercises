3c6eae169e60f78428045c12a8ece6b8c8f2dd00
Len512 <echatzak@gmail.com>
2017-05-15 10:57:27 +0200

    Create AddressCard.h

---
+++ Ch15/15.5/AddressCard.h
@@ -0,0 +1,25 @@
+#ifndef AddressCard_h
+#define AddressCard_h
+#import <Foundation/Foundation.h>
+
+@interface AddressCard : NSObject
+{
+    NSString *firstName;
+    NSString *lastName;
+    NSString *email;
+    NSString *address;
+    NSString *country;
+    NSString *phoneNumber;
+    
+}
+@property NSString *firstName, *email, *lastName, *address, *country, *phoneNumber;
+
+- (id) initWithFirstName: (NSString *) fn withLastName: (NSString *) ln andEmail: (NSString *) e andAddress: (NSString *) a andCountry: (NSString *) c andPhoneNumber: (NSString *) p;
+- (id) init;
+- (void) print;
+- (void) setFirstName: (NSString *) fn withLastName: (NSString *) ln andEmail: (NSString *) e andAddress: (NSString *) a andCountry: (NSString *) c andPhoneNumber: (NSString *) p;
+- (NSString *) name;
+- (NSComparisonResult) compareNames: (id) element;
+- (NSMutableArray *) props;
+@end
+#endif /* AddressCard_h */
