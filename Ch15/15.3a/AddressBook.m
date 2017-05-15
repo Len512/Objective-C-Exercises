d31fb43c0f69beb77f13cb3964d554271203bdcd
Len512 <echatzak@gmail.com>
2017-05-15 09:28:55 +0200

    Create AddressBook.m

---
+++ Ch15/15.3a/AddressBook.m
@@ -0,0 +1,46 @@
+#import <Foundation/Foundation.h>
+#import "AddressBook.h"
+
+@implementation AddressBook
+
+- (id) initWithName:(NSString *) name {
+    self = [super init];
+    if (self) {
+        bookName = [[NSString alloc] initWithString: name];
+        book = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+- (id) init {
+    return [self initWithName:@"NoName"];
+}
+- (void) addCard: (AddressCard *) theCard {
+    [book addObject:theCard];
+}
+- (void) removeCard: (AddressCard *) theCard {
+    [book removeObjectIdenticalTo: theCard];
+}
+- (long int) entries {
+    return [book count];
+}
+- (void) list {
+    NSLog(@"======== Contents of: %@ =========", bookName);
+    for ( AddressCard *theCard in book){
+        NSLog(@"%-20s   %-32s", [theCard.name UTF8String], [theCard.email UTF8String]);
+        NSLog(@"=====================================================");
+    }
+}
+- (void) sort {
+    [book sortUsingSelector:@selector(compareNames:)];
+}
+- (NSMutableArray *) lookup: (NSString *) theName {
+    
+    NSMutableArray *matches = [NSMutableArray array];
+    for (AddressCard *nextCard in book){
+        if ([nextCard.name localizedCaseInsensitiveContainsString:theName]){
+            [matches addObject:nextCard];
+        }
+    }
+    return [matches count] ? matches: nil;
+}
+@end
