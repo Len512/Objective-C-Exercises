ac4c541d1347e103b67d647f2754913b05e73da3
Len512 <echatzak@gmail.com>
2017-05-16 10:41:32 +0200

    Create Program16-2_main.m

---
+++ Ch16/Book Programs/Program16-2_main.m
@@ -0,0 +1,35 @@
+// Make a copy of a file
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+//         NSString *fName = @"/Users/eleni/Documents/Programming/ObjC-Kochan/Ch16/Ch16/testfile";
+//        NSString *new = @"/Users/eleni/Documents/Programming/ObjC-Kochan/Ch16/Ch16/newfile";
+        NSString *new2 = @"/Users/eleni/Documents/Programming/ObjC-Kochan/Ch16/Ch16/newfile2";
+        NSString *new3 = @"/Users/eleni/Documents/Programming/ObjC-Kochan/Ch16/Ch16/newfile3";
+        
+        NSFileManager *fm;
+        NSData *fileData;
+        
+        fm = [NSFileManager defaultManager];
+        
+        // Read newfile2
+        
+        fileData = [fm contentsAtPath:new2];
+        
+        if (fileData == nil){
+            NSLog(@"File read failed!");
+            return 1;
+        }
+        
+        // Write the data to newfile3
+        if ([fm createFileAtPath:new3 contents:fileData attributes:nil] == NO){
+            NSLog(@"Could not create the copy!");
+            return 2;
+        }
+        NSLog(@"File copy was successfull!");
+    }
+    return 0;
+}
