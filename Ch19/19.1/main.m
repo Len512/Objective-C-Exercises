000649f4e249301bf5cf1bebf2d323c452931732
Len512 <echatzak@gmail.com>
2017-05-18 16:07:17 +0200

    Create main.m

---
+++ Ch19/19.1/main.m
@@ -0,0 +1,35 @@
+// In Chapter 15, Program 15.7 generated a table of prime numbers.
+// Modify that program to write the resulting array as an XML property list
+// to the file primes.pl. Then examine the contents of the file.
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int d, isPrime, p;
+        NSMutableArray *primes = [NSMutableArray array];
+        NSArray *result;
+        NSString *filename = @"/Users/eleni/Documents/Programming/ObjC-Kochan/Ch19/Ch19/primes.pl";
+        for (p = 2; p <= 50; ++p) {
+            isPrime = 1;
+            if ( p % 2 != 0 || p == 2){
+                for (d = 2; d < p && isPrime; ++d){
+                    if (p % d == 0){
+                        isPrime = 0;
+                    }
+                }
+                if (isPrime != 0 ){
+                    [primes addObject: [NSNumber numberWithInteger:p]];
+                }
+            }
+        }
+        if ([primes writeToFile:filename atomically:YES] == NO){
+            NSLog(@"Save to file failed!");
+        }
+        
+        result = [NSArray arrayWithContentsOfFile:filename];
+        NSLog(@" %@ ", result);
+        
+    }
+    return 0;
+}
