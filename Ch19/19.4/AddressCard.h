4973d0d0124d1eff703bc17a7945ff666536a97b
Len512 <echatzak@gmail.com>
2017-05-19 10:27:34 +0200

    Create AddressCard.h

---
+++ Ch19/19.4/AddressCard.h
@@ -0,0 +1,19 @@
+#ifndef AddressCard_h
+#define AddressCard_h
+
+#import <Foundation/Foundation.h>
+
+@interface AddressCard : NSObject <NSCopying, NSCoding>
+{
+    NSString *name, *email;
+}
+@property (nonatomic, copy) NSString *name, *email;
+- (id) initWithName:(NSString *) n andEmail:(NSString *) e;
+- (id) init;
+- (void) setName: (NSString *) theName andEmail: (NSString *) theEmail;
+- (void) print;
+- (NSComparisonResult) compareNames: (id) element;
+
+@end
+
+#endif /* AddressCard_h */
